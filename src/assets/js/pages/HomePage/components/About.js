50169b8578a9faacd7daaaf7d6ffd264d72b6baa
Redback <thomsonnicholas@hotmail.com>
2018-02-08 18:42:36 -0800

    Added Event Cards Example

--- src/assets/js/pages/HomePage/components/About.js
+++ src/assets/js/pages/HomePage/components/About.js
@@ -5,7 +5,42 @@ import Hero from '~/components/Hero';
 export default class About extends React.Component {
   render() {
     return (<div className="about">
-      This is the about section.
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  HackXR
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  SDHacks
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  HackXX
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>);
   }
 };
