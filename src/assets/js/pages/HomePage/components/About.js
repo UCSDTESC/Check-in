b65fe219980feeabc2ae17ec0db889699ea5964b
Nancy Vuong <nvuong123@Nancys-MacBook-Pro-4.local>
2018-02-08 18:51:21 -0800

    Fixed Stash Merge

--- src/assets/js/pages/HomePage/components/About.js
+++ src/assets/js/pages/HomePage/components/About.js
@@ -5,8 +5,47 @@ import Hero from '~/components/Hero';
 export default class About extends React.Component {
   render() {
     return (<div className="about">
+<<<<<<< Updated upstream
       <img className="about__logo" src="https://www.sdhacks.io/assets/img/vectors/logo.svg"/>
       This is the about section.
+=======
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  HackXR
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  SDHacks
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img src="#" className="card-img-top" />
+              <div className="card-body">
+                <p className="card-text">
+                  HackXX
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>);
   }
 };
