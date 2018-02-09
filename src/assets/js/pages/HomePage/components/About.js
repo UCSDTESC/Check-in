f86eb13af5fa7fb89bfc77f80d87e89c59371d8c
Nancy Vuong <nvuong123@Nancys-MacBook-Pro-4.local>
2018-02-08 18:44:00 -0800

    Created About styles

--- src/assets/js/pages/HomePage/components/About.js
+++ src/assets/js/pages/HomePage/components/About.js
@@ -5,6 +5,7 @@ import Hero from '~/components/Hero';
 export default class About extends React.Component {
   render() {
     return (<div className="about">
+      <img className="about__logo" src="https://www.sdhacks.io/assets/img/vectors/logo.svg"/>
       This is the about section.
     </div>);
   }
