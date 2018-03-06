ed9905a00dd8fd5f2b1670170216914d17133b45
Subhankar Panda <s1panda@ucsd.edu>
2018-03-06 13:06:28 -0800

    added event descriptions

--- src/assets/js/pages/ApplyPage/index.js
+++ src/assets/js/pages/ApplyPage/index.js
@@ -178,7 +178,7 @@ class ApplyPage extends React.Component {
     };
 
     let validator = createValidator(options);
-
+    console.log(event);
     if (!event) {
       return (<div className="page apply-page apply-page--loading">
         <NavHeader />
@@ -192,6 +192,11 @@ class ApplyPage extends React.Component {
         <div className="sd-form__wrapper">
           <Progress percent={(page * 100) / 4} />
           <div className="sd-form">
+            <div className="row sd-form__row">
+              <div className="col-12 text-center sd-form__desc">
+                {event.description}
+              </div>
+            </div>
             {event && <Header name={event.name} logo={event.logo} />}
             {page === 1 && <PersonalSection onSubmit={this.nextPage}
               validate={validator} event={event} options={options} />}
