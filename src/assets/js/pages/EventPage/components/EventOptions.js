0ca243a7f89230e32d269e0467edee48ed7364ce
Subhankar Panda <s1panda@ucsd.edu>
2018-07-24 23:44:35 -0400

    Add Text For Diversity Event Option

--- src/assets/js/pages/EventPage/components/EventOptions.js
+++ src/assets/js/pages/EventPage/components/EventOptions.js
@@ -41,7 +41,8 @@ export default class EventOptions extends React.Component {
       allowOutOfState: 'Allow Out-of-State Students',
       foodOption : 'Add Food Preference Section',
       requireResume: 'Require Resume Uploads',
-      allowTeammates : 'Allow Team Memebers'
+      allowTeammates : 'Allow Team Memebers',
+      requireDiversityOption: 'Require Race In Application'
     };
 
     const optionDescriptions = {
