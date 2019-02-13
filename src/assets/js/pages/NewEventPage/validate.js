de1304df45c7aa8113de59b13f11a75bc7906f50
Subhankar Panda <s1panda@ucsd.edu>
2019-02-12 17:15:12 -0800

    Added Validator

--- src/assets/js/pages/NewEventPage/validate.js
+++ src/assets/js/pages/NewEventPage/validate.js
@@ -1,4 +1,4 @@
-const createValidator = (options) => (values) => {
+const createValidator = () => (values) => {
   const errors = {};
 
   let required = ['name', 'alias', 'closeTimeMonth', 'closeTimeDay',
@@ -7,7 +7,21 @@ const createValidator = (options) => (values) => {
   const notValid = required.filter(name => !(name in values));
   notValid.forEach(name => errors[name] = 'Required');
 
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
 
+  if (values.closeTimeDay < 1 || values.closeTimeDay > 31) {
+    errors.closeTimeDay = 'Invalid Day';
+  }
+
+  if (values.closeTimeMonth === 'Month' || values.closeTimeMonth < 1 || values.closeTimeMonth > 12) {
+    errors.closeTimeMonth = 'Invalid Month';
+  }
+
+  if (values.closeTimeYear < 1900 + new Date().getYear()) {
+    errors.closeTimeYear = 'Invalid Year';
+  }
 
   return errors;
 };
