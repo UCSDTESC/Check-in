b93596c7eb4247a7d644cc0c2ee52bd3cfe85e5f
Redback <thomsonnicholas@hotmail.com>
2019-05-07 15:10:34 -0700

    Removed Duplicate Endpoints

--- src/server/api/controllers/UserAuthController.ts
+++ src/server/api/controllers/UserAuthController.ts
@@ -1,11 +1,13 @@
 import { USER_JWT_TIMEOUT } from '@Config/Passport';
 import { EventDocument } from '@Models/Event';
+import EmailService from '@Services/EmailService';
 import UserService from '@Services/UserService';
 import { TESCAccount } from '@Shared/ModelTypes';
-import { JWTUserAuthToken, JWTUserAuth } from '@Shared/api/Responses';
+import { ForgotPasswordRequest, ResetPasswordRequest } from '@Shared/api/Requests';
+import { JWTUserAuthToken, JWTUserAuth, SuccessResponse } from '@Shared/api/Responses';
 import { Response, Request } from 'express';
 import * as jwt from 'jsonwebtoken';
-import { Get, JsonController, UseBefore, Res, Req, Post } from 'routing-controllers';
+import { Get, JsonController, UseBefore, Res, Req, Post, Body } from 'routing-controllers';
 
 import { ErrorMessage } from '../../utils/Errors';
 import { AuthorisedUser } from '../decorators/AuthorisedUser';
@@ -17,6 +19,7 @@ import { ValidateEventAlias } from '../middleware/ValidateEventAlias';
 @JsonController('/user')
 export class UserAuthController {
   constructor(
+    private EmailService: EmailService,
     private UserService: UserService
   ) {}
 
@@ -45,6 +48,24 @@ export class UserAuthController {
       token: `JWT ${this.generateToken(jwt)}`,
       user: jwt,
     };
+  }
+
+  @Post('/forgot')
+  async forgotPassword(@Body() body: ForgotPasswordRequest, @Req() req: Request) {
+    const account = await this.UserService.getAccountByEmail(body.email);
+
+    if (!account) {
+      throw new Error(ErrorMessage.NO_ACCOUNT_EXISTS());
+    }
+
+    await this.EmailService.sendPasswordResetEmail(req, account);
+    return SuccessResponse.Positive;
+  }
+
+  @Post('/reset')
+  async resetPassword(@Body() body: ResetPasswordRequest) {
+    await this.UserService.resetUserPassword(body.id, body.newPassword);
+    return SuccessResponse.Positive;
   }
 
   @Get('/current/:eventAlias')
--- src/server/api/controllers/UserController.ts
+++ src/server/api/controllers/UserController.ts
@@ -23,54 +23,8 @@ import { UserDocument } from '@Models/User';
 @JsonController('/user')
 export class UserController {
   constructor(
-    private EmailService: EmailService,
     private UserService: UserService
   ) {}
-
-  /**
-   * Signs a user with the JWT secret.
-   * @param user The public user information to sign.
-   * @returns The JWT token signed for that user.
-   */
-  generateToken(user: JWTUserAuthToken) {
-    return jwt.sign(user, Config.SessionSecret, {
-      expiresIn: USER_JWT_TIMEOUT,
-    });
-  }
-
-  @Get('/authorised')
-  @UseBefore(UserAuthorisation)
-  async authorised(@Res() res: Response) {
-    return res.status(200).end();
-  }
-
-  @Post('/login')
-  @UseBefore(UserLogin)
-  login(@Req() req: Request): JWTUserAuth {
-    const jwt = this.UserService.getJwtUser(req.user);
-    return {
-      token: `JWT ${this.generateToken(jwt)}`,
-      user: jwt,
-    };
-  }
-
-  @Post('/forgot')
-  async forgotPassword(@Body() body: ForgotPasswordRequest, @Req() req: Request) {
-    const account = await this.UserService.getAccountByEmail(body.email);
-
-    if (!account) {
-      throw new Error(ErrorMessage.NO_ACCOUNT_EXISTS());
-    }
-
-    await this.EmailService.sendPasswordResetEmail(req, account);
-    return SuccessResponse.Positive;
-  }
-
-  @Post('/reset')
-  async resetPassword(@Body() body: ResetPasswordRequest) {
-    await this.UserService.resetUserPassword(body.id, body.newPassword);
-    return SuccessResponse.Positive;
-  }
 
   @Get('/events')
   @UseBefore(UserAuthorisation)
