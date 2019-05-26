c7b4585ef29f589a0ece63500041d2dc69c1e95b
Subhankar Panda <s1panda@ucsd.edu>
2019-05-25 17:32:32 -0700

    User and UsersPage done

--- src/client/pages/UsersPage/components/UserList.tsx
+++ src/client/pages/UsersPage/components/UserList.tsx
@@ -10,6 +10,8 @@ import { AlertType } from '../../AlertPage';
 const styles = require('react-table/react-table.css');
 
 interface UserListProps {
+
+  //users that have applied to an event
   users: TESCUser[];
   columns: AutofillColumn[];
   event: TESCEvent;
@@ -17,7 +19,17 @@ interface UserListProps {
   onUserUpdate: (newUser: TESCUser) => void;
 }
 
+/**
+ * This renders the react-table for the users that have applied to an event
+ */
 class UserList extends React.Component<UserListProps> {
+
+  /**
+   * Render the User component, used by react-table's SubComponent
+   * 
+   * @param {TESCUser} row the user to be rendered
+   * @returns {JSXElement}
+   */
   expandComponent = (row: TESCUser) => (
     <div className="user-list__child">
       <User
